9bce1c8995e2dd4526714622c270f32f23a2d554
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-01-05 02:33:56 +0100

    Remove dupplicated TileEntity registration

--- src/main/java/fr/iamacat/Client/clientProxy.java
+++ src/main/java/fr/iamacat/Client/clientProxy.java
@@ -14,13 +14,6 @@ public class clientProxy extends commonProxy {
 	}
 	@Override
 	public void registerTileEntity() {
-		GameRegistry.registerTileEntity(tileEntityUserRegistrationStone.class, "tileEntityUserRegistrationStone");
-        GameRegistry.registerTileEntity(tileAccelerateBlock.class, "tileAccelerateBlock");
-        GameRegistry.registerTileEntity(tilegrowthStone.class, "tilegrowthStone");
-        GameRegistry.registerTileEntity(tileAdvancedAccelerateBlock.class, "tileAdvancedAccelerateBlock");
-        GameRegistry.registerTileEntity(tileAdvancedgrowthStone.class, "tileAdvancedgrowthStone");
-        GameRegistry.registerTileEntity(tileSuperHopper.class, "tileSuperHopper");
-        GameRegistry.registerTileEntity(tileRitualLauncher.class, "tileRitualLauncher");
 	}
 	public void registerEventHandlers() {
 		 super.registerEventHandlers();
